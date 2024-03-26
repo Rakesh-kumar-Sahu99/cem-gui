10cb51a3576b0b8c19d0a7d48989b1027fe36fcc
Rakesh Kumar Sahu <rakesh.sahu92@outlook.com>
2024-03-27 00:14:23 +0530

    colomn names,configuration to the table,data for the table added

--- src/app/data-management/data-source-list/data-source-list.component.ts
+++ src/app/data-management/data-source-list/data-source-list.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { DataSourceFormComponent } from '../data-source-form/data-source-form.component';
+import { ColDef } from 'ag-grid-community';
 
 @Component({
   selector: 'app-data-source-list',
@@ -10,10 +11,45 @@ import { DataSourceFormComponent } from '../data-source-form/data-source-form.co
 export class DataSourceListComponent {
 
   title = "Data Source List";
+  public data:any =[];
+  public colDefs: ColDef[] = [
+    { field: "DB_TYPE" },
+    { field: "HostName" },
+    { field: "Name" },
+    { field: "Port" },
+    {field: "UserName"},
+    {field: "Password"}
+  ];
+
+  public tableConfiguration: ColDef = {
+    flex: 1,
+    minWidth: 150,
+    filter: true,
+    autoHeight: true
+  };
   // public displayForm:boolean=false;
   constructor(private dialog:MatDialog){
+    this.data = [
+        {
+          DB_TYPE : "Oracle",
+          HostName : "localhost",
+          Name:"springboot",
+          Port:"8080",
+          UserName:"root",
+          Password:"root"
+        },
+        {
+          DB_TYPE : "MySQL",
+          HostName : "localhost",
+          Name:"springboot",
+          Port:"8081",
+          UserName:"root",
+          Password:"1234"
+        }]
 
   }
+
+
 
   openDataSourceForm(){
     // this.displayForm=true;
